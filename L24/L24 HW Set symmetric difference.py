156f5c2f7258c6de48eafc77f4dedce0655d322d
aryachowdhury-gif <arya.chowdhury@gmail.com>
2025-12-21 16:42:13 +0530

    L24

---
+++ L24/L24 HW Set symmetric difference.py
@@ -0,0 +1,17 @@
+def sym_difference(set1, set2):
+    print("Original sets:")
+    print(set1)
+    print(set2)
+    print()
+
+    result = set.symmetric_difference(set2)
+    print("Symmetric deffernce of setc1 - setc2")
+    return result
+
+seta1 = set(["green", "blue"])
+seta2 = set(["blue", "yellow"])
+resulta = sym_difference(seta1, seta2)
+
+print("Result of a Set:")
+print(resulta)
+print()
