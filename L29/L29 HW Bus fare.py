24a01d85ec5e001fe396df4dc1b6e9c762c668b4
aryachowdhury-gif <arya.chowdhury@gmail.com>
2026-01-24 17:00:59 +0530

    L29

---
+++ L29/L29 HW Bus fare.py
@@ -0,0 +1,17 @@
+class Vehical:
+    def __init__(self, name, mile, volume):
+        self.name = name
+        self.mileage = mile
+        self.capacity = volume
+
+    def fare(self):
+        return self.capacity * 100
+
+class Bus(Vehical):
+    def fare(self):
+        amount = super().fare()
+        amount += amount  * 10 / 100
+        return amount
+
+School_bus = Bus("School Volvo", 12, 50)
+print("Total Bus fare is:", School_bus.fare())
